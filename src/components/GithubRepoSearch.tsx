ffb87bb0b2d5f654a1968d2a1a1ccc979264b509
frankgomezdev <franklingomezdev@gmail.com>
2026-04-02 13:26:52 -0400

    feat: added pagination

--- src/components/GithubRepoSearch.tsx
+++ src/components/GithubRepoSearch.tsx
@@ -2,13 +2,26 @@ import { useQuery } from "@tanstack/react-query";
 import RepoCard, { type Repository } from "./RepoCard";
 import "../index.css"
 import { useState } from "react";
+import Pagination from "./Pagination";
 
 interface GitHubResponse {
  items: Repository[];
+ total_count: number;
 }
 
-const fetchRepo = async(searchInput: string) => {
-    const response = await fetch(`https://api.github.com/search/repositories?q=${searchInput}`);
+interface SearchParams {
+    searchInput: string;
+    page: number;
+    perPage: number;
+}
+
+const fetchRepo = async(params: SearchParams) => {
+    const urlParams = new URLSearchParams({
+        q: params.searchInput,
+        page: params.page.toString(),
+        per_page: params.perPage.toString(),
+    })
+    const response = await fetch(`https://api.github.com/search/repositories?q=${urlParams}`);
     if(!response.ok){
         throw new Error('Unable to fetch repositories.')
     }
@@ -16,10 +29,13 @@ const fetchRepo = async(searchInput: string) => {
 }
 
 function GithubRepoSearch() {
+    const [page, setPage] = useState(1);
+    const perPage = 10;
     const [searchInput, setSearchInput] = useState("");
-    const { data: repositoryData, isLoading, isError } = useQuery<GitHubResponse>({
-        queryKey: ['repositories', searchInput],
-        queryFn: () => fetchRepo(searchInput),
+    const params = {searchInput, page, perPage}
+    const { data: repositoryData, isLoading, isError, error } = useQuery<GitHubResponse>({
+        queryKey: ['repositories', params],
+        queryFn: () => fetchRepo(params),
         enabled: searchInput !== "",
     })
 
@@ -35,14 +51,22 @@ function GithubRepoSearch() {
                     name="searchInput"
                     type="text"
                     placeholder="Search for repositories.."
-                    className="w-full border border-gray-900 bg-gray-100 p-2 mb-3 rounded"
+                    className="w-full border border-gray-900 bg-gray-100 p-2 mb-2 rounded"
                 />
             </form>
             {isLoading && <p>Loading...</p>}
-            {isError && <p>No repositories found!</p>}
+            {isError && <p>An error has occurred: {error.message}</p>}
                 {repositoryData?.items?.map((repository) => (
                     <RepoCard key={repository.id} repository={repository} />
                 ))}
+                {repositoryData && (
+                    <Pagination 
+                        currentPage={page}
+                        perPage={perPage}
+                        totalCount={repositoryData.total_count}
+                        onPageChange={setPage}
+                    />
+                )}
         </div>
     )
 }
---
+++ src/components/Pagination.tsx
@@ -0,0 +1,45 @@
+interface PageProps {
+    currentPage: number;
+    perPage: number;
+    totalCount: number;
+    onPageChange: (p: number) => void;
+}
+
+function Pagination({ currentPage, perPage, totalCount, onPageChange }: PageProps) {
+  const numberOfPages = Math.ceil(totalCount / perPage);
+  const allPages = [];
+  for (let i = 1; i <= numberOfPages; i++) {
+    allPages.push(i);
+  }
+  const lastPage =
+    allPages[allPages.length - 1] > 100 ? 100 : allPages[allPages.length - 1];
+  const startIndex = currentPage - 3 > 1 ? currentPage - 3 : 1;
+  const endIndex = currentPage + 2;
+  const middlePages = allPages.slice(startIndex, endIndex);
+  const visiblePages = [allPages[0], ...middlePages, lastPage];
+
+  return (
+    <ul className="flex gap-3 my-3 justify-center">
+      {visiblePages.map((page, index) => {
+        const isCurrentPage = currentPage === page;
+        const buttonClass = isCurrentPage
+          ? "cursor-pointer rounded-xl py-3 px-3 font-bold bg-blue-400 text-white"
+          : "cursor-pointer rounded-xl py-3 px-3 hover:bg-gray-400";
+
+        const nextPage = visiblePages[index + 1];
+        const showEllipsis = nextPage - page > 1;
+
+        return (
+          <li key={page}>
+            <button onClick={() => onPageChange(page)} className={buttonClass}>
+              {page}
+            </button>
+            {showEllipsis && <span>...</span>}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default Pagination;
